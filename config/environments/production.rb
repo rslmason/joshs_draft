bd95d8b8b3aa9613516a4ba9f64778d3650cd2ab
rslmason <rslmason@outlook.com>
2024-02-04 15:04:07 -0500

    configure secret key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+
+  Rails.application.configure do
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  end
 end
